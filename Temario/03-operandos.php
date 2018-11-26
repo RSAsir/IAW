a826dc248ef3237e27266d9dc2d1cded23108569
RSAsir <sprubencasir@gmail.com>
2018-11-26 18:22:45 +0100

    Actualizado fichero operandos

--- Temario/03-operandos.php
+++ Temario/03-operandos.php
@@ -1,9 +1,26 @@
 <?php
 
-	// Variables
-		$operando1 = "1";
-		$operando2 = "3";
-		$suma = $operando1 + $operando2;
-	// Suma
-		echo "El valor de la suma es $suma.";
+	// Operadores aritmeticos
+		$x = 6;
+		$y = 2;
+
+		echo $x + $y . "<br>"; # Suma
+		echo $x - $y . "<br>"; # Resta
+		echo $x * $y . "<br>"; # Multiplicacion
+		echo $x / $y . "<br>"; # Division
+		echo $x % $y . "<br>"; # Modulo (resto de la division)
+		echo $x ** $y . "<br>"; # Potencia
+
+	# Operadores de comparacion
+		/* A parte de los comunes, estan:
+			== : Devuelve true si $x y $y son iguales.
+			=== : Devuelve true si el valor y el tipo de datos de $x y $y es igual.
+			!==: No identico, es la negacion de lo anterior. */
+
+	# Operadores logicos
+		/* Existen los siguientes:
+			and o &&: Cierto si $x y $y son true.
+			or o ||: Cierto si $x o $y es true.
+			xor: Cierto si $x o $y es true pero no ambos.
+			! : Cierto si $x no es true. */
 ?>
