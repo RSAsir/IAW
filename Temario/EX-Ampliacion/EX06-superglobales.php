7364b84bf69d35f8e045a2aac91cad0fbf05692e
RSAsir <sprubencasir@gmail.com>
2018-11-26 18:59:09 +0100

    Subido fichero variables superglobales

---
+++ Temario/EX-Ampliacion/EX06-superglobales.php
@@ -0,0 +1,28 @@
+<?php
+
+    # Son variables predefinidas del leguanje a las que siempre se puede acceder.
+    # (Posteriormente se añadiran mas)
+
+    # 1. $GLOBALS
+        # Es una variable que se utiliza para acceder a todas las otras variables globales definidas por el usuario.
+            $x = 67;
+            $y = 34;
+
+            function testGLOBALS() {
+                echo "<h3>Superglobal \$GLOBALS:</h3>";
+                echo $GLOBALS['x'] + $GLOBALS['y'];
+            }
+
+            testGLOBALS();
+    
+    # 2. $_REQUEST
+        # Toma datos despues de que se han subido mediante un formulario html.
+
+    # 3. $_POST
+        # Toma datos despues de que se han subido mediante un formulario que usa "method = post".
+        # Tambien se usa para pasar variables.
+    
+    # 3. $_GET
+        # Toma datos despues de que se han subido mediante un formulario que usa "method = get".
+        # Tambien puede tomar informacion enviada en la url.
+?>
